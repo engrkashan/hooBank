577d0de6cc90f51ba80bc8e77c68f518d737407e
Kashee Umar <kshfkhl@gmail.com>
2023-04-28 00:54:23 +0500

    init commit

--- src/components/Getstarted.jsx
+++ src/components/Getstarted.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import styles from "../style";
+import { arrowUp } from "../assets";
 
 const Getstarted = () => {
-  return <div>Getstarted</div>;
+  return (
+    <div
+      className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
+    >
+      <div
+        className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}
+      >
+        <div className={`${styles.flexStart} flex-rwo`}>
+          <p className="font-poppins font-medium text-[18px] leadaing-[23px] mr-2">
+            <span className="text-gradient">Get</span>
+          </p>
+          <img
+            src={arrowUp}
+            alt="arrow"
+            className="w-[23px] h-[23] object-contain"
+          />
+        </div>
+
+        <p className="font-poppins font-medium text-[18px] leadaing-[23px]">
+          <span className="text-gradient">Get</span>
+          <span className="text-gradient">Started</span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Getstarted;
