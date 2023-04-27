97f9f920cb5478b1b46e4a094e0685790fe3c76c
Kashee Umar <kshfkhl@gmail.com>
2023-04-27 21:39:27 +0500

    init commit

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Billings from "./Billings";
+import Button from "./Button";
+import CTA from "./CTA";
+import Clients from "./Clients";
+import Footer from "./Footer";
+import FeedbackCard from "./FeedbackCard";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import CardDeal from "./CardDeal.jsx";
+import Getstarted from "./Getstarted";
+import Business from "./Business";
+
+export {
+  Navbar,
+  Billings,
+  Business,
+  Hero,
+  FeedbackCard,
+  Stats,
+  Testimonials,
+  Getstarted,
+  CardDeal,
+  CTA,
+  Clients,
+  Button,
+  Footer,
+};
